83001898ac3600874089d0f256d1ae89a9fbcdae
stevenzz <gomezacunasneyder@gmail.com>
2026-06-19 22:52:02 -0500

    Sincronizar estructura limpia de archivos

--- main.js
+++ main.js
@@ -19,24 +19,49 @@ loadPokemons();
 
 const pokemonSelected = async (pokemonUrl) => {
     try {
-
-        const response = await fetch(pokemonUrl).then(response => response.json());
-
         const pokemonImage = document.getElementById("pokemon-image");
         const pokemonName = document.getElementById("pokemon-name");
         const pokemonStats = document.getElementById("pokemon-stats");
+        const pokemonabilities = document.getElementById("pokemon-abilities");
+        const pokemonclass = document.getElementById("pokemonclass");
+
+        pokemonName.textContent = "";
+        pokemonImage.removeAttribute("src");
+        pokemonStats.innerHTML = "";
+        pokemonabilities.innerHTML = "";
+        pokemonclass.textContent = "";
+
+        const response = await fetch(pokemonUrl).then(response => response.json());
 
         pokemonImage.src = response.sprites.front_default;
         pokemonName.textContent = response.name;
-
-        pokemonStats.innerHTML = "";
 
         response.stats.forEach(stat => {
             const li = document.createElement("li");
             li.textContent = `${stat.stat.name}: ${stat.base_stat}`;
             pokemonStats.appendChild(li);
-
         })
+
+        response.abilities.forEach(ability => {
+            const li = document.createElement("li");
+            li.textContent = ability.ability.name;
+            pokemonabilities.appendChild(li);
+        })
+
+        pokemonImage.addEventListener("mouseenter", () => {
+           let html ="tipo de pokemon: <br>"
+            for (let i = 0; i < response.types.length; i ++){
+                  html+= ` ${response.types[i].type.name}</h3>`;
+            }
+            pokemonclass.innerHTML = html;
+        });
+
+        pokemonImage.addEventListener("mouseleave", () => {
+            pokemonclass.innerHTML = "";
+        });
+
+        console.log(response);
+       
     } catch (error) {
         console.error("Error fetching pokemon details:", error);
     }
